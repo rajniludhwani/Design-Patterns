7a6c16a1930739952892f7b4b189db605300a762
rajniludhwani <ludhwani.rajni@gmail.com>
2024-01-08 19:52:54 +0530

    Minor Update

--- Proxy-Pattern/src/main/java/com/java/design/patterns/Main.java
+++ Proxy-Pattern/src/main/java/com/java/design/patterns/Main.java
@@ -5,6 +5,7 @@ public class Main {
         try {
             EmployeeDao empTableObj = new EmployeeDaoProxy();
             empTableObj.create("ADMIN", new EmployeeDo());
+            System.out.println("Operation Successful");
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
